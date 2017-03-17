3a8cae706f65b72d1c13a772144c94a23ced3df2
Harshit Sidhwa <harshit.sidhwa@gmail.com>
2017-03-17 19:19:41 +0530

    Update background.js
    Added Keyboard shortcut Feature

--- background.js
+++ background.js
@@ -5,6 +5,14 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   });
 });
 
+chrome.commands.onCommand.addListener(function(tab) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    // alert("Shortcut Executed");
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
